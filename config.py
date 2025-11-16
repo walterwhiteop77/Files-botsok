7cfe196989dc2acb268d35b78303d5a953a37326
walterwhiteop77 <walterwhite2k27@gmail.com>
2025-11-16 21:56:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -24,7 +24,7 @@ DB_NAME = "Cluster0"
 
 FSUBS = [[-1001610198839, True, 10]] # Force Subscription Channels [channel_id, request_enabled, timer_in_minutes]
 # Database Channel (Primary)
-DB_CHANNEL = -1003330908686   # just put channel id dont add ""
+DB_CHANNEL =-1003330908686   # just put channel id dont add ""
 # Multiple Database Channels (can be set via bot settings)
 # DB_CHANNELS = {
 #     "-1002595092736": {"name": "Primary DB", "is_primary": True, "is_active": True},
